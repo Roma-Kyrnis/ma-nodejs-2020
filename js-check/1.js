cac88a9520bd91d11623745730a46663fcbc258a
KyrnosRoman <kirnos007@gmail.com>
2019-10-24 18:52:02 +0300

    1

--- js-check/1.js
+++ js-check/1.js
@@ -1,1 +1,13 @@
+var my_number = -1;
+var myNumber = 0;
+var Number = 3;
 
+if (true){
+    my_number = 1;
+    let myNumber = 2;
+    Number = 6;
+}
+
+const sum = my_number + myNumber + Number;
+
+console.log(sum);
