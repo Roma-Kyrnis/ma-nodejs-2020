522e7f1505c487f0309dfc94b4fa5107be02a0d9
Roman <kirnos007@gmail.com>
2019-11-21 12:31:31 +0200

    delete

--- homework-02/src/task/index.js
+++
@@ -1,5 +0,0 @@
-const task1 = require('./task1.js');
-const task2 = require('./task2.js');
-const task3 = require('./task3.js');
-
-module.exports = { sum: task1, earth: task2, setTime: task3 };
--- homework-02/src/task/task3.js
+++
@@ -1,8 +0,0 @@
-function setTime(timer = 1000, text = 'resolved') {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(text);
-    }, timer);
-  });
-}
-module.exports = setTime(3000, 'Some text.').then((result) => console.log(result));
