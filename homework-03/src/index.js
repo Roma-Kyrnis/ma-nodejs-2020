aead1acddd9980963856c51c8f3ba47cf1f2365a
Roman <kirnos007@gmail.com>
2019-12-05 14:24:35 +0200

    fix, many time spend with for in the bigest number

--- homework-03/src/index.js
+++ homework-03/src/index.js
@@ -1,20 +1,29 @@
-const result = [];
-let i = 0;
-function isPrime(num) {
-  for (let j = 2; j < num; j++) {
-    if (num % (j * j) === 0) return false;
-    if (num % j === 0) return false;
+let result = 0;
+let number = 10;
+let divisor = 2;
+function isPrime() {
+  for (let i = 1; i <= 100; i++) {
+    if (number === divisor || divisor > Math.sqrt(number)) {
+      divisor = 2;
+      return true;
+    }
+    if (number % divisor === 0) {
+      divisor = 2;
+      number += 1;
+      break;
+    }
+    divisor += 1;
   }
-  return num > 1;
+  return false;
 }
+
 setInterval(() => {
-  if (isPrime(i)) {
-    result.push(i);
+  if (isPrime()) {
+    result = number;
+    number += 1;
   }
-  i += 1;
 }, 0);
+
 setInterval(() => {
-  console.log(
-    `${Date.now()} : --IN PROCESS-- Biggest prime number found: ${result[result.length - 1]}`,
-  );
+  console.log(`${Date.now()} : --IN PROCESS-- Biggest prime number found: ${result}`);
 }, 1000);
