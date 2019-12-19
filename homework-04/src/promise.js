140bddf3bb44b81e65d2244317c293a875a2b696
Roman <kirnos007@gmail.com>
2019-12-19 13:53:29 +0200

    try to improve

--- homework-04/src/promise.js
+++ homework-04/src/promise.js
@@ -12,25 +12,45 @@ function delay(callback, ms) {
     }, ms);
   });
 }
-let one;
-let two;
+// let one;
+// let two;
 
 async function dice() {
   console.log(`Data is ${Date.now()} start`);
-  throwDice().then((res) => {
-    one = res;
-    delay(() => {
-      console.log(`Date is ${Date.now()}, number one ${res}`);
-    }, 700);
-  });
-  throwDice().then((res) => {
-    two = res;
-    delay(() => {
-      console.log(`Date is ${Date.now()}, number two ${res}`);
-    }, 2000);
-  });
+
   delay(() => {
-    console.log(`Data is ${Date.now()} sum: ${one + two}`);
-  }, 3000);
+    throwDice(() => {
+      delay(() => {
+        throwDice(() => {
+          console.log();
+        });
+      });
+    });
+  })
+    .then((res) => {
+      return res;
+    })
+    .then((res) => {
+      return res;
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.error(err));
+  // throwDice().then((res) => {
+  //   one = res;
+  //   delay(() => {
+  //     console.log(`Date is ${Date.now()}, number one ${res}`);
+  //   }, 700);
+  // });
+  // throwDice().then((res) => {
+  //   two = res;
+  //   delay(() => {
+  //     console.log(`Date is ${Date.now()}, number two ${res}`);
+  //   }, 2000);
+  // });
+  // delay(() => {
+  //   console.log(`Data is ${Date.now()} sum: ${one + two}`);
+  // }, 3000);
 }
 dice();
