5950d67c56edca3921d914d00035d27de5fb290f
Roman <kirnos007@gmail.com>
2019-12-21 00:19:24 +0200

    did flag

--- homework-06/src/index.js
+++ homework-06/src/index.js
@@ -1,8 +1,8 @@
 const os = require('os');
 
-let rateInput;
-let limitInput;
-let colorInput;
+let rateInput = 1000;
+let limitInput = 300;
+let colorInput = 'true';
 let totalMem;
 let freeMem;
 let allocatedMem;
@@ -11,13 +11,14 @@ let colorDelta = '%s';
 let colorAttention = '%s';
 
 process.argv.forEach((element) => {
-  const [, value] = element.split('=');
-  if (/^RATE=\d+$/.test(element)) rateInput = value;
-  if (/^LIMIT=\d+$/.test(element)) limitInput = value;
-  if (/^COLOR=(true|false)$/.test(element)) colorInput = value;
+  const [key, value] = element.split('=');
+  if (/^RATE=\d+$/.test(element) || (key === '--rate' && /^\d+$/.test(value))) rateInput = value;
+  if (/^LIMIT=\d+$/.test(element) || (key === '--limit' && /^\d+$/.test(value))) limitInput = value;
+  if (/^COLOR=(true|false)$/.test(element) || (key === '--color' && /^(true|false)$/.test(value)))
+    colorInput = value;
 });
 
-function cliTool(rate = 10000, limit = 300, color = true) {
+function cliTool(rate, limit, color) {
   console.clear();
 
   totalMem = Math.round(os.totalmem() / 1e6);
@@ -25,11 +26,10 @@ function cliTool(rate = 10000, limit = 300, color = true) {
   delta = (allocatedMem - totalMem - freeMem).toFixed(3);
   allocatedMem = (totalMem - freeMem).toFixed(3);
 
-  if (color === true) {
+  if (color === 'true') {
     if (delta < 0) colorDelta = '\x1b[31m%s\x1b[0m';
     else colorDelta = '\x1b[32m%s\x1b[0m';
     if (Number(freeMem) < limit) colorAttention = '\x1b[31m%s\x1b[0m';
-    else colorAttention = '\x1b[32m%s\x1b[0m';
   }
 
   console.log(`Total system memory: ${totalMem} MB`);
@@ -37,9 +37,8 @@ function cliTool(rate = 10000, limit = 300, color = true) {
   console.log(`Allocated memory: ${allocatedMem} MB`);
   console.log(`Delta for previous allocated memory value: ${colorDelta} MB`, delta);
 
-  if (Number(freeMem) < limit)
+  if (freeMem < limit)
     console.log(colorAttention, '!!! ATTENTION: Available memory is under the defined limit !!!');
-  console.log(Date.now());
 
   setTimeout(() => {
     cliTool(rateInput, limitInput, colorInput);
