9afcf71dbe2f901dfc64840ad702f3acc021071a
Roman <kirnos007@gmail.com>
2019-12-21 19:22:16 +0200

    promise did

--- homework-04/src/promise.js
+++ homework-04/src/promise.js
@@ -5,6 +5,7 @@ function throwDice() {
     else resolve(result);
   });
 }
+
 function delay(callback, ms) {
   return new Promise(() => {
     setTimeout(() => {
@@ -12,63 +13,30 @@ function delay(callback, ms) {
     }, ms);
   });
 }
-// let one;
-// let two;
+
+let one;
+let two;
 
 async function dice() {
   // console.log(`${Date.now()} One: ${one}`);
-
-  delay(() => {
-    console.log(`${Date.now()} Data is  start`);
-    return throwDice();
-  }, 0)
+  console.log(`Data is ${Date.now()} start`);
+  throwDice()
     .then((res) => {
-      console.log(res);
+      one = res;
       delay(() => {
-        console.log(res);
-        console.log(`Two: ${Date.now()}`);
-      }, 1300);
+        console.log(`Date is ${Date.now()}, number one ${res}`);
+      }, 700);
+      return throwDice();
     })
-    .then(
+    .then((res) => {
+      two = res;
       delay(() => {
-        console.log(`Sum: ${Date.now()}`);
-      }, 1000))
+        console.log(`Date is ${Date.now()}, number two ${res}`);
+      }, 2000);
+    })
+    .then(() => {
+      delay(() => console.log(`Data is ${Date.now()} sum: ${one + two}`), 3000);
+    })
     .catch((err) => console.error(`In error: ${err}`));
-
-  // delay(() => {
-  //   throwDice(() => {
-  //     delay(() => {
-  //       throwDice(() => {
-  //         console.log();
-  //       });
-  //     });
-  //   });
-  // })
-  //   .then((res) => {
-  //     return res;
-  //   })
-  //   .then((res) => {
-  //     return res;
-  //   })
-  //   .then((res) => {
-  //     console.log(res);
-  //   })
-  //   .catch((err) => console.error(err));
-
-  // throwDice().then((res) => {
-  //   one = res;
-  //   delay(() => {
-  //     console.log(`Date is ${Date.now()}, number one ${res}`);
-  //   }, 700);
-  // });
-  // throwDice().then((res) => {
-  //   two = res;
-  //   delay(() => {
-  //     console.log(`Date is ${Date.now()}, number two ${res}`);
-  //   }, 2000);
-  // });
-  // delay(() => {
-  //   console.log(`Data is ${Date.now()} sum: ${one + two}`);
-  // }, 3000);
 }
 dice();
