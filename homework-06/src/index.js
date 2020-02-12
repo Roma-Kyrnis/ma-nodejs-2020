6eaf9d9e1b33686d0d57925de3080df508d77b0d
Roman-Kirnos <kirnos007@gmail.com>
2020-02-12 09:15:04 +0200

    corrected all comments

--- homework-06/src/index.js
+++ homework-06/src/index.js
@@ -3,17 +3,15 @@ const os = require('os');
 let rateInput = 1000;
 let limitInput = 300;
 let colorInput = true;
-let totalMem;
-let freeMem;
 let allocatedMem;
-let delta;
-let colorDelta = '%s';
-let colorAttention = '%s';
 
 process.argv.forEach((element) => {
   const [key, value] = element.split('=');
-  if (/^RATE=\d+$/.test(element) || (key === '--rate' && /^\d+$/.test(value))) rateInput = value;
-  if (/^LIMIT=\d+$/.test(element) || (key === '--limit' && /^\d+$/.test(value))) limitInput = value;
+  const parsedValue = parseFloat(value);
+  if (/^RATE=\d+$/.test(element) || (key === '--rate' && /^\d+$/.test(value)))
+    rateInput = parsedValue;
+  if (/^LIMIT=\d+$/.test(element) || (key === '--limit' && /^\d+$/.test(value)))
+    limitInput = parsedValue;
   if (/^COLOR=(true|false)$/.test(element) || (key === '--color' && /^(true|false)$/.test(value))) {
     if (value === 'true') colorInput = true;
     else colorInput = false;
@@ -23,21 +21,23 @@ process.argv.forEach((element) => {
 function cliTool(rate, limit, color) {
   console.clear();
 
-  totalMem = Math.round(os.totalmem() / 1e6);
-  freeMem = (os.freemem() / 1e6).toFixed(3);
-  delta = (allocatedMem - totalMem - freeMem).toFixed(3);
-  allocatedMem = (totalMem - freeMem).toFixed(3);
+  let colorDelta = '%s';
+  let colorAttention = '%s';
+  const totalMem = Math.round(os.totalmem() / 1e6);
+  const freeMem = os.freemem() / 1e6;
+  const delta = allocatedMem - totalMem - freeMem;
+  allocatedMem = totalMem - freeMem;
 
   if (color) {
     if (delta < 0) colorDelta = '\x1b[31m%s\x1b[0m';
     else colorDelta = '\x1b[32m%s\x1b[0m';
-    if (Number(freeMem) < limit) colorAttention = '\x1b[31m%s\x1b[0m';
+    if (freeMem < limit) colorAttention = '\x1b[31m%s\x1b[0m';
   }
 
   console.log(`Total system memory: ${totalMem} MB`);
-  console.log(`Free memory available: ${colorAttention} MB`, freeMem);
-  console.log(`Allocated memory: ${allocatedMem} MB`);
-  console.log(`Delta for previous allocated memory value: ${colorDelta} MB`, delta);
+  console.log(`Free memory available: ${colorAttention} MB`, freeMem.toFixed(3));
+  console.log(`Allocated memory: ${allocatedMem.toFixed(3)} MB`);
+  console.log(`Delta for previous allocated memory value: ${colorDelta} MB`, delta.toFixed(3));
 
   if (freeMem < limit)
     console.log(colorAttention, '!!! ATTENTION: Available memory is under the defined limit !!!');
